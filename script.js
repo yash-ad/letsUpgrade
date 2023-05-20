b8a9eca6a96bff6ebd7f019b4b729c52c85abcfe
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-05-20 09:27:20 +0530

    Created an assignment of letsupgrade Bootcamp

---
+++ script.js
@@ -0,0 +1,13 @@
+//Create a function that takes two numbers as parameters and returns their sum:-
+
+//1.Here lets create a function to addNumbers respectively
+
+function addNumbers(num1,num2)
+{
+ return num1 + num2;
+
+}
+
+let result = addNumbers(5,6);
+
+console.log(result);
