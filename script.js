7682131cc380c14dd00b139d00d292c0328a4c20
Yashraj Dandnaik <102809742+yash-ad@users.noreply.github.com>
2023-05-22 10:15:52 +0530

    Letsupgrade assignement  created an array of fruits

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function addNumbers(num1,num2)
 let result = addNumbers(5,6);
 
 console.log(result);
+
+
+
+//2.1.Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
+
+//.Use JavaScript to display the total number of fruits in the array
+
+
+const fruits = ["apple", "banana", "orange", "grape", "mango"]
+
+console.log(`Total Number Of Fruits ${fruits.length}`);
